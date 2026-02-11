941b5d743b0dd23f8956a0a65923b82d6804c419
PhysicsND <nayakdawit@outlook.com>
2026-02-11 14:38:44 -0800

    added Cam switcher

--- P7PlayerControlDawit/Assets/Scripts/PlayerController.cs
+++ P7PlayerControlDawit/Assets/Scripts/PlayerController.cs
@@ -2,21 +2,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+    public float speed = 20f;
+    public float turnSpeed = 45f;
+
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey = KeyCode.F;
+
     private float horizontalInput;
     private float forwardInput;
 
     void Update()
     {
-        // Get player input
+        // Axis setup
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        // Move the vehicle forward/backward
+        // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-        // Turn the vehicle
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        // Rotate the vehicle left and right
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        // Switch cameras
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
