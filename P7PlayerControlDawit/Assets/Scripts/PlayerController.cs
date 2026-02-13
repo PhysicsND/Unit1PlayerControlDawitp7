81f582b811a21c58c0cebd56c60a642018238532
PhysicsND <nayakdawit@outlook.com>
2026-02-13 15:21:59 -0800

    made 2 player compatibble

--- P7PlayerControlDawit/Assets/Scripts/PlayerController.cs
+++ P7PlayerControlDawit/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,24 @@ public class PlayerController : MonoBehaviour
     public float speed = 20f;
     public float turnSpeed = 45f;
 
+    public string inputID;          // "1" or "2"
     public Camera mainCamera;
     public Camera hoodCamera;
-    public KeyCode switchKey = KeyCode.F;
+    public KeyCode switchKey;
 
     private float horizontalInput;
     private float forwardInput;
 
     void Update()
     {
-        // Axis setup
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        // Axis setup (based on player number)
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
 
-        // Move the vehicle forward
+        // Move the vehicle forward/back
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-        // Rotate the vehicle left and right
+        // Rotate the vehicle left/right
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
         // Switch cameras
