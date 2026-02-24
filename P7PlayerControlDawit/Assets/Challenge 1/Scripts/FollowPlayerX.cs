47d86ff678521cf3e4786d50573a0e7750ea6e29
PhysicsND <nayakdawit@outlook.com>
2026-02-24 15:19:02 -0800

    Did challenge 1
    fixed da plane
    
    Co-Authored-By: d1luffy <128961239+d1luffy@users.noreply.github.com>

---
+++ P7PlayerControlDawit/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    // LateUpdate runs after all Update functions
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
